86799acee851e15d98fa3780606bb065cf8d9d8b
Benjamin Franzke <benjaminfranzke@gmail.com>
2020-04-20 19:31:45 +0200

    Version 0.2 that introduces DI in localconf

---
+++ Classes/ExampleService.php
@@ -0,0 +1,23 @@
+<?php
+namespace Bnf\DiInLocalconf;
+
+use TYPO3\CMS\Core\Configuration\SiteConfiguration;
+
+/**
+ * ExampleService
+ *
+ * @author Benjamin Franzke <bfr@qbus.de>
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class ExampleService
+{
+
+    public function __construct(SiteConfiguration $siteConfiguration)
+    {
+        return null;
+    }
+
+    public function doFoo(): void
+    {
+    }
+}
---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+//\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bnf\DiInLocalconf\ExampleService::class)->doFoo();
