985bdc25f05748f4a6bbf622e9ea382108b3c5bc
Benjamin Franzke <benjaminfranzke@gmail.com>
2020-04-20 19:29:55 +0200

    Initial version without DI

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Demo Dependency Injection usage in localconf',
+    'category' => '',
+    'author' => 'Benjamin Franzke',
+    'author_email' => 'benjaminfranzke@gmail.com',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.1.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '10.3.0-10.4.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'Bnf\\DiInLocalconf\\' => 'Classes',
+        ),
+    ),
+);
