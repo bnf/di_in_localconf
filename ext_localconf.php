df5b08a1a3e4ab5b895200a8922d3ebbf53c4b83
Benjamin Franzke <benjaminfranzke@gmail.com>
2020-04-21 09:58:06 +0200

    Release v0.3.0

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-//\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bnf\DiInLocalconf\ExampleService::class)->doFoo();
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bnf\DiInLocalconf\ExampleService::class)->doFoo();
